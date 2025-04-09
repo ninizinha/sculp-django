2457385b9c1bc4a7e712e8cdd9dc8500dff41de2
ninizinha <stapmk12@gmail.com>
2025-04-09 09:56:41 -0300

    Colocado o endereço no ALLOWHOST de novo

--- projeto_sculp/settings.py
+++ projeto_sculp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_$2oq4!)1m%6!!%ngv&58eg0s99&5&z!=@u^pym+jhbj&%pe7n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['sculp-django.onrender.com']
+ALLOWED_HOSTS = ['sculp-django.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
